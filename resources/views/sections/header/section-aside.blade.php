d7dc00f66db612cf0aff1d33bcf5b685b49826eb
Javier Soler Artero <javier.soler.jsa@gmail.com>
2024-01-02 16:16:53 +0100

    Menu nice version

--- resources/views/sections/header/section-aside.blade.php
+++ resources/views/sections/header/section-aside.blade.php
@@ -1,3 +1,3 @@
-<div class="hidden lg:flex lg:flex-1 lg:justify-end">
+<div class="flex flex-1 justify-end">
     <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Log in</a>
 </div>
