20661b2f9392bbfbfa74bfca6497fd09f4ef6c95
Javier Soler Artero <javier.soler.jsa@gmail.com>
2024-02-06 21:08:37 +0100

    Added migrationsç

--- packages/package-customers/src/Model/CustomerAttributes.php
+++ packages/package-customers/src/Model/CustomerAttributes.php
@@ -7,6 +7,8 @@ use Wzrd\Customer\Model\CustomerAttributesValues;
 
 class CustomerAttributes extends Model
 {
+    protected $table = 'customer_attributes';
+
     protected $fillable = [
         'code', 'label', 'frontend_type', 'backend_type'
     ];
