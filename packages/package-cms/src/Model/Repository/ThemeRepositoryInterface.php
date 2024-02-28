2977c47afd480a51220bbad6c6923d6a75ccc80a
Javier Soler Artero <javier.soler.jsa@gmail.com>
2024-02-28 16:46:56 +0100

    WzrdCms Clean

--- packages/package-cms/src/Model/Repository/ThemeRepositoryInterface.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Wzrd\Cms\Model\Repository;
-
-interface ThemeRepositoryInterface
-{
-    public function allTheme();
-}
--- packages/package-cms/src/WzrdCmsProvider.php
+++ packages/package-cms/src/WzrdCmsProvider.php
@@ -2,19 +2,19 @@
 
 namespace Wzrd\Cms;
 
+use Illuminate\Support\Facades\App;
 use Spatie\LaravelPackageTools\Commands\InstallCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 use Wzrd\Cms\Filament\Resources\CmsResource\Fields\DynamicField;
 use Wzrd\Cms\Filament\Resources\CmsResource\Fields\DynamicFieldInterface;
+use Wzrd\Cms\Filament\Resources\CmsResource\Pages\Theme\ThemeEdit;
 use Wzrd\Cms\Model\Api\BlockInterface;
 use Wzrd\Cms\Model\Api\CmsServiceInterface;
 use Wzrd\Cms\Model\Api\ViewBuilderInterface;
 use Wzrd\Cms\Model\Implement\Block;
 use Wzrd\Cms\Model\Implement\CmsService;
 use Wzrd\Cms\Model\Implement\ViewBuilder;
-use Wzrd\Cms\Model\Repository\ThemeRepository;
-use Wzrd\Cms\Model\Repository\ThemeRepositoryInterface;
 
 class WzrdCmsProvider extends PackageServiceProvider
 {
@@ -39,7 +39,6 @@ class WzrdCmsProvider extends PackageServiceProvider
         $this->app->bind(CmsServiceInterface::class, CmsService::class);
         $this->app->bind(ViewBuilderInterface::class, ViewBuilder::class);
         $this->app->bind(BlockInterface::class, Block::class);
-        $this->app->bind(ThemeRepositoryInterface::class, ThemeRepository::class);
         $this->app->bind(DynamicFieldInterface::class, DynamicField::class);
     }
 
