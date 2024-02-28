2d3f452fdbb416faf7ba7e737ce66bf9b55b88ef
Javier Soler Artero <javier.soler.jsa@gmail.com>
2024-02-28 19:45:13 +0100

    Logs git issue

--- packages/filament-login-logs/src/Resources/LoginLogResource/LoginLogResource.php
+++ packages/filament-login-logs/src/Resources/LoginLogResource/LoginLogResource.php
@@ -12,7 +12,7 @@ class LoginLogResource extends Resource
 {
     protected static ?string $model = LoginLog::class;
     protected static ?int $navigationSort = 100;
-    protected static ?string $navigationIcon = 'heroicon-o-lock-closed';
+    protected static ?string $navigationIcon = 'filetype-s-log';
 
     public static function getNavigationGroup(): ?string
     {
