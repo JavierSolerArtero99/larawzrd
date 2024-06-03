bad35b85045ea143747dac90a01ae6cff03ea9ca
Javier Soler Artero <javier.soler.jsa@gmail.com>
2024-06-03 19:58:19 +0200

    Mr OwnPage

--- packages/project-calandratex-domain/src/Filament/CtOrder/Pages/CtOrderIndex.php
+++ packages/project-calandratex-domain/src/Filament/CtOrder/Pages/CtOrderIndex.php
@@ -2,17 +2,18 @@
 
 namespace Wzrd\CalandraTexDomain\Filament\CtOrder\Pages;
 
-use Filament\Resources\Pages\ListRecords;
+use Filament\Resources\Pages\Page;
 use Filament\Tables\Table;
 use Wzrd\CalandraTexDomain\Filament\CtOrder\Resources\CtOrderResource;
 use Wzrd\CalandraTexDomain\Filament\CtOrder\Widgets\CtData;
 use Wzrd\CalandraTexDomain\Filament\CtOrder\Widgets\CtOrderHistory;
 use Wzrd\CalandraTexDomain\Filament\CtOrder\Widgets\NewCtOrder;
 
-class CtOrderIndex extends ListRecords
+class CtOrderIndex extends Page
 {
     protected static string $resource = CtOrderResource::class;
     protected static ?string $title = "CalandraTex";
+    protected static string $view = 'CtDomain::admin.pages.index-calandratex';
 
     public function getHeaderWidgetsColumns(): int|string|array
     {
